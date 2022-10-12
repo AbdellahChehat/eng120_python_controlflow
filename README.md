9250a8ec5cac4d5320b88c641e5e88b01665e4ac
Faduma Muse <faduma@Fadumas-MacBook-Pro.local>
2022-10-12 14:19:20 +0100

    update

---
+++ README.md
@@ -0,0 +1,10 @@
+# Python Control flow
+## if elif else
+### loops -for loop - while loop 
+#### Python keywords break - continue etc 
+
+# if it's cold:
+     take jacket
+# if its raining 
+     rain mack
+# if it's sunny : boolean value - true or false 
---
+++ control_flow.py
@@ -0,0 +1,31 @@
+#
+
+
+weather = "cold"
+# if it's cold:
+if weather == "cold" : #if true or false
+    print("wear a jacket")
+elif weather == "sunny" :
+    print("lets go to the beach")
+
+else:
+    print("No match found better luck next time")
+
+     #rain mack
+
+# else or elif
+# Age restriction for movie ticket
+# Create a condition for 18 & above
+# Create 16 & above
+# universal
+# pg
+# 12a
+# 15 and above
+# if nothing matched inform the user to enter age again
+# user must not be allowed to anter age over 117 years
+
+Age = 18
+age1 = 16
+age2 = 12
+age3 = 15
+
